a8c94c89e3486d5b0f5aa6c501110c1eb73114c2
sz8386pr <sz8386pr>
2018-01-17 15:24:22 -0600

    Issue #7: sort the list option added before book list display

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from book import Book
+import operator
 
 
 def display_menu_get_choice():
@@ -18,6 +19,28 @@ def display_menu_get_choice():
 
     return choice
 
+def sort_by():
+    ''' book display sorting option '''
+
+    while True:
+        print('''
+            Sort by...
+            1. Book ID
+            2. Book title
+            3. Book author
+        ''')
+
+        choice = input('Enter your selection: ')
+
+        if choice == "1":
+            return "id"
+        elif choice == "2":
+            return "title"
+        elif choice == "3":
+            return "author"
+        else:
+            message('Please enter a valid selection')
+
 
 def show_list(books):
     ''' Format and display a list of book objects'''
@@ -26,10 +49,20 @@ def show_list(books):
         print ('* No books *')
         return
 
-    for book in books:
-        print(book)
+    sorted_list(books) # Display sorted book list
 
     print('* {} book(s) *'.format(len(books)))
+
+
+def sorted_list(books):
+    ''' sort the book list by user order
+    reference from https://stackoverflow.com/questions/4010322/sort-a-list-of-class-instances-python for sort a list of class instances'''
+
+    sort = sort_by() # Get the sort option
+    ordered_books = sorted(books, key=operator.attrgetter(sort)) # Sort by id/title/author. Original list is unaltered by using sorted() method
+
+    for book in ordered_books:
+        print(book)
 
 
 def ask_for_book_id():
